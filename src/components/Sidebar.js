9f99e99c8ad82aa1e8da65e73370ae8bc5072b04
zyar3k <a.zygan@gmail.com>
2022-02-28 16:18:27 +0100

    Add sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import CreateEventButton from "./CreateEventButton";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <aside className="border p-5 w-64">
+      <CreateEventButton />
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
